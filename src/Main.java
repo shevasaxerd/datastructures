93feafe151add96ab75075d6c5c13ef8a303f5a6
shevasaxerd <shevasaxerd@gmail.com>
2024-09-23 17:01:24 +0300

    Added class CustomArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,40 @@
-
+import models.CustomArrayList;
 
 
 public class Main {
     public static void main(String[] args) {
+        CustomArrayList<String> list1 = new CustomArrayList<>();
+        CustomArrayList<Integer> list2 = new CustomArrayList<>();
+        list1.add("Banana");
+        list1.add("Apple");
+        list1.add("Orange");
+        list1.add("Zakhar");
+        list2.add(52);
+        list2.add(394);
+        list2.add(1000-7);
+        list2.add(69);
+
+        System.out.println("List1: ");
+        list1.print();
+        System.out.println("List2: ");
+        list2.print();
 
 
+        list1.remove("Apple");
+        list2.remove(394);
+        list1.print();
+        list2.print();
+
+
+        System.out.println("Does the list1 contain Bannana? " + list1.contains("Banana"));
+        System.out.println("Does the list2 contain 52? " + list2.contains(52));
+        System.out.println("Does the list1 contain March? " + list1.contains("March"));
+        System.out.println("Does the list2 contain 12? " + list2.contains(12));
+
+        list1.sort(String::compareTo);
+        list2.sort(Integer::compareTo);
+        System.out.println("After sorting:");
+        list1.print();
+        list2.print();
     }
 }
--- src/models/CustomArrayList.java
+++ src/models/CustomArrayList.java
@@ -1,4 +1,71 @@
 package models;
 
-public class CustomArrayList {
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class CustomArrayList<T> {
+    private Object[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public CustomArrayList() {
+        elements = new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void add(T element) {
+        ensureCapacity();
+        elements[size++] = element;
+    }
+
+    public boolean remove(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                System.arraycopy(elements, i + 1, elements, i, size - i - 1);
+                elements[--size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean contains(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Неверный индекс: " + index);
+        }
+        return (T) elements[index];
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(elements[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    public void sort(Comparator<? super T> comparator) {
+        Arrays.sort((T[]) elements, 0, size, comparator);
+    }
+
+    public int size() {
+        return size;
+    }
+
+
 }
